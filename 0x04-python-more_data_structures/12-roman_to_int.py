185ec7cd168691c75bd94c28cca43ee71801ae1e
Tabitht <imafidontabitha@gmail.com>
2023-08-16 23:59:31 +0000

    corrected one more file

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
 def roman_value(main_string):
     """ To assign the numbers to the right roman numeral """
-    roman = [('I', 1), ('V', 5), ('X', 10),('L', 50), ('C', 100), ('D', 500), ('M', 1000)]
+    roman = [('I', 1), ('V', 5), ('X', 10),
+             ('L', 50), ('C', 100), ('D', 500), ('M', 1000)]
     for items in roman:
         key, element = items
         if main_string is key:
             return element
     return None
+
+
 def next_roman(numeral, index):
-    """ checks the next character in the string and assigns the right number to the given roman numeral """
+    """ checks the next character in the string a
+    nd assigns the right number to the given roman numeral """
     if index + 1 < len(numeral):
         return roman_value(numeral[index + 1])
     else:
         return None
+
+
 def roman_to_int(roman_string):
     result = 0
     if (roman_string is None or isinstance(roman_string, str) is False):
