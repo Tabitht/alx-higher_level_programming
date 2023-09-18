0ce857e25f834d8485d65d3a165b16449a63611a
Tabitht <imafidontabitha@gmail.com>
2023-09-18 19:55:07 +0000

    new changes

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -8,6 +8,6 @@ class Base:
     def __init__(self, id=None):
         if id != None:
             self.id = id
-        else
-        Base.__nb_objects += 1
-        self.id = Base.__nb_objects
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
