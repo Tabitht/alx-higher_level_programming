62d51ee34f7266431d2398b1da44724648026347
Tabitht <imafidontabitha@gmail.com>
2024-01-07 20:54:26 +0000

    made corrections to file 5-text_indentation.py along with the docfile test

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -8,8 +8,16 @@ def text_indentation(text):
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    for i in text:
-        if i == "\n" or i in ".?:":
-            print("\n")
-        else:
-            print(i, end='')
+    count = 0
+    while count < len(text) and text[count] == ' ':
+        count += 1
+    while count < len(text):
+        print(text[count], end="")
+        if text[count] == "\n" or text[count] in ".?:":
+            if text[count] in ".?:":
+                print("\n")
+            count += 1
+            while count < len(text) and text[count] == ' ':
+                count += 1
+            continue
+        count += 1
